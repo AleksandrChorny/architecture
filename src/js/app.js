3e4a31a3516edb028ca1ed2f67fbcb3ec9d2e2a5
AleksandrChorny <99405017+AleksandrChorny@users.noreply.github.com>
2022-04-08 15:46:02 +0300

    Co-authored-by: Andrii Bondar <BondarAS@users.noreply.github.com>

--- src/js/app.js
+++ src/js/app.js
@@ -10,8 +10,4 @@ flsFunctions.isWebp();
 //! const swiper = new Swiper(...);
 
 //Виклик функції додавання каласу в н6алащтуваннях передаємо ім'я класу по якому клік я до кого потрібно додати '_action'
-click.clickOnClassADDClassName('.burger-menu', '.main-menu');
-click.clickOnClassADDClassName('.burger-menu', '.footer');
-//console.log(click.clickOnClass());
-
-//console.log('action');
+click.clickOnClassADDClassName('.burger-menu', '.header__menu');
